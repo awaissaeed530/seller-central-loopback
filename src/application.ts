1ac33003f178e7c4e4c320288f23fd8278ce93cc
Awais Saeed <awaissaeed530@gmail.com>
2022-07-06 04:01:06 +0500

    add sqs queue

--- src/application.ts
+++ src/application.ts
@@ -7,6 +7,12 @@ import {
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
+import {SQS} from 'aws-sdk';
+import {
+  SqsConsumerBindings,
+  SqsConsumerComponent,
+  SqsConsumerConfig,
+} from 'loopback4-sqs-consumer';
 import path from 'path';
 import {environment} from './environments';
 import {SP_CLIENT} from './keys';
@@ -20,21 +26,19 @@ export class SellerCentralApplication extends BootMixin(
   constructor(options: ApplicationConfig = {}) {
     super(options);
 
-    // Set up the custom sequence
     this.sequence(MySequence);
 
-    // Set up default home page
     this.static('/', path.join(__dirname, '../public'));
 
-    // Customize @loopback/rest-explorer configuration here
     this.configure(RestExplorerBindings.COMPONENT).to({
       path: '/explorer',
     });
     this.component(RestExplorerComponent);
+
     this.configure(SP_CLIENT).to(environment.SP_CONFIG);
+    this.configureSQSConsumer();
 
     this.projectRoot = __dirname;
-    // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
       controllers: {
         // Customize ControllerBooter Conventions here
@@ -43,5 +47,17 @@ export class SellerCentralApplication extends BootMixin(
         nested: true,
       },
     };
+  }
+
+  private configureSQSConsumer() {
+    const sqsConfig: SqsConsumerConfig = {
+      sqs: new SQS({
+        region: environment.AWS_CONFIG.REGION,
+        accessKeyId: environment.AWS_CONFIG.ACCESS_KEY_ID,
+        secretAccessKey: environment.AWS_CONFIG.SECRET_ACCESS_KEY,
+      }),
+    };
+    this.configure(SqsConsumerBindings.COMPONENT).to(sqsConfig);
+    this.component(SqsConsumerComponent);
   }
 }
